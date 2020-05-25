9c002f656e2fa4d99ae7494b6d4f7b674322bbd5
sevval-armagan <svvlarmagan@gmail.com>
2020-05-25 17:32:28 +0300

    First Commit

--- MoviesApplication/Controller/SegmentedVC.swift
+++ MoviesApplication/Controller/SegmentedVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 private let movies = HomePageVC()
 class SegmentedVC: UIViewController{
-   
+   //Burak
     
     fileprivate let segmentedControl : UISegmentedControl = {
         let sc = UISegmentedControl()
